ba18edc0b686e23f633d6d20e85b1f57c3dc914d
noahpoz <noahpocz@gmail.com>
2017-09-26 23:59:47 -0400

    blue step button

--- src/view/navbar/index.js
+++ src/view/navbar/index.js
@@ -6,7 +6,7 @@ import '../styles/index.scss'
 const Navbar = () => (
     <Card raised={true} style={{width: '100%', height: '100%'}}>
       <div style={{height: '100%', display: 'flex', alignItems: 'center', padding: '15px'}}>
-        <Button> Step </Button>
+        <Button primary> Step </Button>
       </div>
     </Card>
 )
