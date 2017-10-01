6b31983c037295797fe44b3666e56619cd031e33
Angus Lam <angushtlam@gmail.com>
2017-09-30 21:43:56 -0400

    Added p5js to project and included a dummy p5 sketch.

---
+++ src/view/visualizer/p5_wrapper/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import p5 from 'p5'
+
+export default class P5Wrapper extends React.Component {
+  componentDidMount() {
+    this.canvas = new p5(this.props.sketch, this.refs.wrapper)
+  }
+
+  render() {
+    return <div id="visualizer" ref="wrapper" style={{ height: '100%', width: '100%', }}></div>
+  }
+}
