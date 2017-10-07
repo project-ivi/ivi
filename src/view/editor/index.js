d644437a65cf1f8abdd6a596512adcab36a093b7
noahpoz <noahpoczciwinski@gmail.com>
2017-10-07 17:37:44 -0400

    remove extraneous code that was causing warnings

--- src/view/editor/index.js
+++ src/view/editor/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { Form, TextArea, Card } from 'semantic-ui-react'
-
-import brace from 'brace';
+import { Card } from 'semantic-ui-react'
 import AceEditor from 'react-ace';
 
 import 'brace/mode/javascript';
 import 'brace/theme/chrome';
 
 class Editor extends React.Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {code: ''};
-	}
 
 	componentDidMount() {
 		// get the ace DOM element
@@ -22,9 +14,6 @@ class Editor extends React.Component {
 		/* Disable automated syntax checking (for now).
 		We may want to implement our own at some point. */
 		this.editor.setUseWorker(false);
-
-		const ace = require('brace')
-		const Range = ace.acequire('ace/range').Range;
 	}
 
 	render() {
@@ -35,11 +24,7 @@ class Editor extends React.Component {
 					ref="ace"
 					style={{width: '100%', height: '100%'}}
 					mode="javascript" 
-					theme="chrome" 
-					onChange={(text) => {
-						// unconventional, but ace will not work if we re-render on every keystroke
-						this.state.code = text;
-					}} />
+					theme="chrome" />
 			</Card>
 		);
 	}
