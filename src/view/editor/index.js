9640d61988e0fcef90865622c708b95396f344e0
noahpoz <noahpoczciwinski@gmail.com>
2017-10-07 22:37:54 -0400

    increase default font size

--- src/view/editor/index.js
+++ src/view/editor/index.js
@@ -5,15 +5,21 @@ import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/chrome';
 
+// constants
+const fontSize = 15;
+
 class Editor extends React.Component {
 
 	componentDidMount() {
 		// get the ace DOM element
-		this.editor = this.refs.ace.editor.getSession();
+		this.editor = this.refs.ace.editor;
 
 		/* Disable automated syntax checking (for now).
 		We may want to implement our own at some point. */
-		this.editor.setUseWorker(false);
+		this.editor.getSession().setUseWorker(false);
+
+		// other defaults
+		this.editor.setFontSize(fontSize)
 	}
 
 	render() {
