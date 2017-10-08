91d74c900613919afee597a5ee725c5e06a30093
Angus Lam <angushtlam@gmail.com>
2017-10-08 18:06:44 -0400

    Updated interpreter module to export functions for React app

--- src/interpreter/executor/interpret.js
+++ src/interpreter/executor/interpret.js
@@ -51,7 +51,7 @@ let isBlockComment = false;
 let userCode = []
 
 // Setup for when we initially check syntax
-function setup(inputCode) {
+export function setup(inputCode) {
     
     //Lock  the code editor
     isRunning = true;
@@ -346,7 +346,7 @@ function findBufferState(buffer) {
 }
 
 // Global evaluate function called on the step and run click
-function evaluate(inputCode) {
+export function evaluate(inputCode) {
    
     //Cleanup our structures before processing
     cleanStructures();
