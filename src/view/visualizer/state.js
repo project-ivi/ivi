e89cbf9263dc9ce3dc3548f444b5eef4e63430bf
Angus Lam <angushtlam@gmail.com>
2017-10-08 18:06:54 -0400

    Connected navbar to use interpreter for program evaluations.

---
+++ src/view/visualizer/state.js
@@ -0,0 +1,22 @@
+const state = {}
+
+export function getSketchState() {
+    return state
+}
+
+export function putInterpreterStep(step) {
+    const actions = step[1]
+    for (const action of actions) {
+        if (action.value === 'null') {
+            delete state[action.name]
+        } else {
+            state[action.name] = action.value
+        }
+    }
+}
+
+export function clearSketchState() {
+    Object.keys(state).forEach(val => {
+        delete state[val]
+    })
+}
