fbd0f5e1fbce6ed1622e8e6a9333f29ae31f1088
noahpoz <noahpoczciwinski@gmail.com>
2017-10-08 21:47:13 -0400

    console output starter

--- src/view/console/index.js
+++ src/view/console/index.js
@@ -6,16 +6,39 @@ const textAreaStyle = {
 	borderColor: 'transparent',
 	backgroundColor:'black',
 	color: 'white',
-	fontFamily: 'Courier'
+	fontFamily: 'Courier',
+	overflow: 'scroll'
 }
 
-const Console = () => (
-  <Card raised={true} style={{height: '100%', width: '100%'}}>
-		<Form style={{height: '100%'}}>
-			<TextArea autoHeight placeholder='>' readOnly style={textAreaStyle} />
-		</Form>
-  </Card>
-)
+class Console extends React.Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.addLine = this.addLine.bind(this);
+		this.state = {consoleContents: ['o']}
+	}
+
+	componentDidMount() {
+		console.log('this happened')
+		this.setState({consoleContents: ['okay']})
+	}
+
+	addLine(line) {
+	}
+	
+	render() {
+		return (
+			<Card raised={true} style={{height: '100%', width: '100%'}}>
+				<Form style={{height: '100%'}}>
+					<TextArea autoHeight placeholder='>' readOnly style={textAreaStyle}>
+						{this.state.consoleContents}
+					</TextArea>
+				</Form>
+	  		</Card>
+		)
+	}
+}
 
 
 export default Console
