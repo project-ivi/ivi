8c5d8944f579a0adcefb48c59efaa65f94874b22
noahpoz <noahpoczciwinski@gmail.com>
2017-10-09 18:21:34 -0400

    fix page resizing, console padding

--- src/view/console/index.js
+++ src/view/console/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 const textAreaStyle = {
-  minHeight: '100%',
-  borderColor: 'transparent',
-  backgroundColor:'black',
-  color: 'white',
-  fontFamily: 'Courier',
+	minHeight: '100%',
+	borderColor: 'transparent',
+	backgroundColor:'black',
+	color: 'white',
+	fontFamily: 'Courier',
   overflow: 'scroll',
-};
+  padding: '13px'
+}
 
 const Console = ({ consoleOutput }) => (
   <Card raised={ true } style={{ height: '100%', width: '100%' }}>
