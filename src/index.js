f1000930cd9f966d3046c7a6ee401a6e802a7e5a
Angus Lam <angushtlam@gmail.com>
2017-10-22 21:35:12 -0400

    Reimplemented Webpack and Build Process

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Interpreter from './view';
-import registerServiceWorker from './registerServiceWorker';
 
 import '../semantic/dist/semantic.min.css';
 
 ReactDOM.render(<Interpreter />, document.getElementById('root'));
-registerServiceWorker();
