52b650a8706fabb1589cc76badc2028886f326af
Angus Lam <angushtlam@gmail.com>
2017-11-04 00:41:59 -0400

    Updated interpreter to support new executor

---
+++ src/interpreter/executor/classes.js
@@ -0,0 +1,40 @@
+// Class representing console output
+export class Console {
+  constructor() {
+    this.output = '';
+  }
+}
+
+// Class representing basic expression
+export class Expression {
+  constructor(derivedFrom) {
+    this.numLines = 0;
+    this.data = null;
+    this.derivedFrom = derivedFrom;
+  }
+}
+
+// Class representing syntax error
+export class Syntax {
+  constructor(error) {
+    this.value = 'Syntax error caused by: ' + error;
+    this.lineNum = '';
+  }
+}
+
+// Class representing unsupported code
+export class Unsupported {
+  constructor(error) {
+    this.value = 'Unsupported: ' + error;
+    this.lineNum = '';
+  }
+}
+
+// Class representing a variable
+export class Variable {
+  constructor() {
+    this.name = '';
+    this.value = 'undefined';
+    this.inferredType = '';
+  }
+}
