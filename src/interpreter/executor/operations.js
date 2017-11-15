54565d60da172f03b27e2c441568f0fb2ca4dbd2
zsmoore <zsmoore@zachary-moore.com>
2017-11-15 16:41:14 -0500

    Variable value getter now working, linted all files

--- src/interpreter/executor/operations.js
+++ src/interpreter/executor/operations.js
@@ -1,15 +1,16 @@
-import { deriveType, stripIfString } from './util';
+import { deriveType, getValueIfVariable } from './util';
 import { typeEnum } from './enums';
 
 export function addition(left, right) {
   let leftType = deriveType(left);
   let rightType = deriveType(right);
+
   if (leftType === typeEnum.STRING || rightType === typeEnum.STRING) {
-    return '\'' + String(stripIfString(left)) + String(stripIfString(right)) + '\'';
+    return '\'' + String(getValueIfVariable(left)) + String(getValueIfVariable(right)) + '\'';
   } else if (leftType === typeEnum.NAN || rightType === typeEnum.NAN) {
     return 'NaN';
   } else if (leftType === typeEnum.NUMBER && rightType === typeEnum.NUMBER) {
-    return String(Number(left) + Number(right));
+    return String(Number(getValueIfVariable(left)) + Number(getValueIfVariable(right)));
   } else { //Handle undefined and all other undefined cases
     return 'NaN';
   }
@@ -19,7 +20,7 @@ export function subtraction(left, right) {
   let leftType = deriveType(left);
   let rightType = deriveType(right);
   if (leftType === typeEnum.NUMBER && rightType === typeEnum.NUMBER) {
-    return String(Number(left) - Number(right));
+    return String(Number(getValueIfVariable(left)) - Number(getValueIfVariable(right)));
   } else {
     return 'NaN';
   }
